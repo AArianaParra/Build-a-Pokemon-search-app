97101295c07ef3486ab88603a519e2e0df16078b
Arianamade4 <arimadeleine496@gmail.com>
2024-06-04 22:39:26 -0400

    Project almost ready

--- script.js
+++ script.js
@@ -26,19 +26,56 @@ const fetchData = async (input) => {
     const response = await fetch(allPokemonList);
     const data = await response.json();
     //console.log(data); Only to check whether the data is well-fetched
+    checkIdOrName(input, data);
+    //return data;
 
   } catch (err) {
     console.log(err);
   }
 }
-//fetchData()
+
 
 //-----------------------
 //Ponerle brillito especial a los cards :)
+const checkIdOrName = (input, data) => {
+  
+  const inputTry = Number(input);
 
+  if (inputTry) {
+    //Here we are gonna search for the ID (use inputTry)
+    searchId(data, input);
+  }
+  else {
+    input = input.toLowerCase(); //It also works with hyphens
+    searchName(data, input);
+    console.log(input);
+        //Here we are gonna search for the name
+  }
+}
 
+//-----------------------
+const searchId = (data, userId) => {
+  const {results} = data;
+  const pokemon = results.find((e) => {
+    const {id} = e;
+    return id === userId; // 'return' needs to be added here due to multiple lines
+  });
+  console.log(pokemon);
+} 
 
+fetchData(124);
 
+//-----------------------
+const searchName = (data, userName) => {
+  const {results} = data;
+  const pokemon = results.find((e) => {
+    const {name} = e;
+    return name === userName; // Agrega 'return' aquí
+  });
+  console.log(pokemon);
+} 
+
+fetchData("scyther");
 /* Events */
 //-----------------------
 //searchButton.addEventListener("click", checkUserInput)
@@ -47,3 +84,5 @@ searchInput.addEventListener("keydown", (e) => {
     console.log("It works!!!")
   }
 })
+
+
